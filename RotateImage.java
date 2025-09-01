272b441627c29374c567e895cad7d9420a65427b
Maharajan07 <maharajan03007@gmail.com>
2025-09-01 15:34:02 +0530

    Rotate the image by 90 degrees

--- RotateImage.java
+++ RotateImage.java
@@ -71,3 +71,67 @@ class Solution {
     }
 }
 */
+
+/*
+for 270 degrees:
+
+class solution {
+    public void rotate270(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the 2D Array
+        for (int i=0; i<n; i++) {
+            for (int j=i; j<n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each column of the 2D Array
+        for (int j=0; j<n; j++) {
+            for (int i=0; i<n/2; i++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-i][j];
+                matrix[n-1-i][j] = temp;
+            }
+        }
+    }
+}
+
+for 180 degrees:
+
+class solution {
+    public void rotate180(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i=0; i<n/2; i++) {
+            for (int j=0; j<n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = temp;
+            }
+        }
+        
+        // if odd dimension, reverse the middle row
+        if (n % 2 == 0) {
+            int mid = n/2;
+            for (int j=0; j<n/2; j++) {
+                int temp = matrix[mid][j];
+                matrix[mid][j] = matrix[mid][n-1-j];
+                matrix[mid][n-1-j] = temp;
+            }
+        }
+    }
+}
+
+To achive any degrees of rotation without changing the loops:
+
+int m = degree / 90;  // how many times to rotate 90°
+
+while (m >= 1) {      // repeat rotation m times
+    rotate90(matrix);   // rotate by 90°
+    m--;
+}
+
+ */
